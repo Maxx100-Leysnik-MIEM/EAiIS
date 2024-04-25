ddbc9ba2d8d2cb3965502b6764996b2f7bef6311
Трескин Никита Михайлович <treskin456@gmail.com>
2024-04-25 15:53:09 +0300

    add route for new device

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template
 from config import HOST, PORT
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder="static/",
+            static_url_path="/static/")
 
 MAIN_ROUTE = "index.html"
 TAKE_ROUTE = "take_return.html"
+DEVICE_ROUTE = "new_device.html"
 
 @app.route('/')
 def index():
@@ -13,6 +16,10 @@ def index():
 @app.route('/newTakeRequest')
 def new_record():
     return render_template(TAKE_ROUTE)
+
+@app.route('/newDevice')
+def new_device():
+    return render_template(DEVICE_ROUTE)
 
 if __name__ == "__main__":
     app.run(HOST, PORT)
