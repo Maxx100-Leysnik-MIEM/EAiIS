729a9028f611ae0ee1039995d43822936f582e86
Трескин Никита Михайлович <treskin456@gmail.com>
2024-05-25 19:22:00 +0300

    update api client to actual

--- openapi_client.py
+++ openapi_client.py
@@ -28,54 +28,50 @@ class DbSession:
                                       headers=headers, params=params, data=data)
         return resp
 
-    def post(self, link: str, headers: dict = None, params: dict = None, data: dict = None) -> requests.models.Response:
-        if headers is None:
-            headers = {'Authorization': f'Bearer {self.JWT_TOKEN}'}
-        else:
-            headers = {'Authorization': f'Bearer {self.JWT_TOKEN}', **headers}
-        resp = requests.post(f"{self.URL}/{link}",
-                             headers=headers, params=params, data=data)
-        return resp
-
     def whoami(self):  # get info about your token
         return self.resp("get_me")
+
+    def get_requests(self, _id: int = None):
+        if _id is None:
+            return self.resp("request")
+        return self.resp(f"request/{str(_id)}")
+
+    def create_request(self):
+        return self.resp("request", method="POST")
+
+    def update_request(self, _id: int):
+        return self.resp(f"request/{str(_id)}", method="PATCH")
+
+    def take_item(self, _id: int):
+        return self.resp(f"request/{str(_id)}/take", method="POST")
+
+    def return_item(self, _id: int):
+        return self.resp(f"request/{str(_id)}/complete", method="POST")
+
+    def cancel_request(self, _id: int):
+        return self.resp(f"request/{str(_id)}/cancel", method="POST")
+
+    def approve_request(self, _id: int):
+        return self.resp(f"request/{str(_id)}/approve", method="PATCH")
 
     def get_user(self, _id: int = None):  # get users list or user by id
         if _id is None:
             return self.resp("users")
         return self.resp(f"users/{str(_id)}")
 
-    def add_user(self):  # TODO
-        return
-
-    def patch_user(self, _id: int):  # TODO
-        return
-
-    def delete_user(self, _id: int):  # TODO
-        return
+    def get_user_rooms(self, _id: int):
+        return self.resp(f"users/{str(_id)}/rooms")
 
     def get_hardware(self, _id: int = None):  # get hardware by id or all list
         if _id is None:
             return self.resp("hardware")
         return self.resp(f"hardware/{str(_id)}")
 
-    def create_hardware(self):  # TODO
-        return
-
     def get_hardware_remains(self):
         return self.resp("hardware/remains")
 
     def get_hardware_types(self):
         return self.resp("hardware/types")
-
-    def add_hardware_type(self):  # TODO
-        return
-
-    def patch_hardware(self, _id: int):  # TODO
-        return
-
-    def delete_hardware(self, _id: int):  # TODO
-        return
 
     """
     GET:    hardware/types/_id (Get Type)
@@ -88,98 +84,35 @@ class DbSession:
             return self.resp("item")
         return self.resp(f"item/{str(_id)}")
 
-    def create_item(self):  # TODO
-        return
-
-    def update_item(self, _id: int):  # TODO
-        return
-
-    def delete_item(self, _id: int):  # TODO
-        return
-
     def get_terminals(self, _id: int = None):  # get terminal by id or all list
         if _id is None:
             return self.resp("terminal")
         return self.resp(f"terminal/{str(_id)}")
-
-    def create_terminal(self):  # TODO
-        return
-
-    def update_terminal(self, _id: int):  # TODO
-        return
-
-    def delete_terminal(self, _id: int):  # TODO
-        return
 
     def get_sections(self, _id: int = None):  # get section by id or all list
         if _id is None:
             return self.resp("section")
         return self.resp(f"section/{str(_id)}")
 
-    def create_section(self):  # TODO
-        return
-
-    def update_section(self, _id: int):  # TODO
-        return
-
-    def delete_section(self, _id: int):  # TODO
-        return
-
     def get_rooms(self, _id: int = None):  # get room by id or all list
         if _id is None:
             return self.resp("room")
         return self.resp(f"room/{str(_id)}")
-
-    def create_room(self):  # TODO
-        return
-
-    def update_room(self, _id: int):  # TODO
-        return
-
-    def delete_room(self, _id: int):  # TODO
-        return
 
     def get_places(self, _id: int = None):  # get place by id or all list
         if _id is None:
             return self.resp("place")
         return self.resp(f"place/{str(_id)}")
 
-    def create_place(self):  # TODO
-        return
-
-    def update_place(self, _id: int):  # TODO
-        return
-
-    def delete_place(self, _id: int):  # TODO
-        return
-
     def get_labs(self, _id: int = None):  # get lab by id or all list
         if _id is None:
             return self.resp("lab")
         return self.resp(f"lab/{str(_id)}")
 
-    def create_lab(self):  # TODO
-        return
-
-    def update_lab(self, _id: int):  # TODO
-        return
-
-    def delete_lab(self, _id: int):  # TODO
-        return
-
     def get_buildings(self, _id: int = None):  # get building by id or all list
         if _id is None:
             return self.resp("building")
         return self.resp(f"building/{str(_id)}")
-
-    def create_building(self):  # TODO
-        return
-
-    def update_building(self, _id: int):  # TODO
-        return
-
-    def delete_building(self, _id: int):  # TODO
-        return
 
     def auth_callback(self):
         return self.resp("auth/callback")
@@ -189,4 +122,3 @@ class DbSession:
 
 
 db = DbSession()
-print(db.get_user().text)  # as example
